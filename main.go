2215dd38cc5219484e0d26ca4ad0a33fa68fab34
Armando Banuelos <abanuelo@Armandos-MacBook-Pro.local>
2023-12-26 11:38:10 -0800

    Adding cohort tier based priority queue

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"container/heap"
 	"context"
+	"fmt"
 	"log"
 	"net"
 
 	"github.com/abanuelo/cinnamon-go/cinnamon"
+	"github.com/abanuelo/cinnamon-go/priorityq"
 	"google.golang.org/grpc"
 )
 
@@ -20,7 +23,20 @@ func (s myCinnamonServer) Create(ctx context.Context, req *cinnamon.CreateReques
 	}, nil
 }
 
+// Total of 768 different priorities, initialized to middle
+// 0-5 tiers where 0 > 5 and 0-127 cohorts where 0 > 127
+var TIER_COHORT_THRESHOLD = 384
+
 func main() {
+	pq := make(priorityq.PriorityQueue, 0)
+
+	heap.Push(&pq, &priorityq.Item{Value: "Item 1", Priority: 3})
+
+	for pq.Len() > 0 {
+		item := heap.Pop(&pq).(*priorityq.Item)
+		fmt.Printf("Popped: %s (Priority: %d)\n", item.Value, item.Priority)
+	}
+
 	lis, err := net.Listen("tcp", ":8089")
 	if err != nil {
 		log.Fatalf("Cannot create listener: %s", err)
---
+++ priorityq/priorityq.go
@@ -0,0 +1,38 @@
+package priorityq
+
+// Item represents an item in the priority queue.
+type Item struct {
+	Value    interface{}
+	Priority int
+}
+
+// PriorityQueue is a min-heap implementation.
+type PriorityQueue []*Item
+
+// Len returns the length of the priority queue.
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+// Less compares two items based on priority.
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].Priority < pq[j].Priority
+}
+
+// Swap swaps two items in the priority queue.
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+// Push adds an item to the priority queue.
+func (pq *PriorityQueue) Push(x interface{}) {
+	item := x.(*Item)
+	*pq = append(*pq, item)
+}
+
+// Pop removes and returns the minimum item from the priority queue.
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	*pq = old[0 : n-1]
+	return item
+}
